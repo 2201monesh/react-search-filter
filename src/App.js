468bda08c44d7ba09b288ffa164d1295ffef5f6f
Monesh Goyal <goyalmonesh2201@gmail.com>
2022-11-20 11:48:11 +0530

    Final changes done

--- src/App.js
+++ src/App.js
@@ -18,12 +18,6 @@ function App() {
     <div className="App">
       <input className='search' placeholder='search here' type="text" onChange={e => setQuery(e.target.value)} />
 
-      {/* <ul className="list">
-      {Users.filter(user => user.first_name.toLowerCase().includes(search)).map(user => (
-        <li key={user.id} className="listItem">{user.first_name}</li>
-      ))}
-      </ul> */}
-
       <Table data={search(Users)} />
     </div>
   );
